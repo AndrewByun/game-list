9992b64e695597f61b0c3ee0bdce8ef00b3ab0b4
Andrew Byun <andrewbyun1@gmail.com>
2024-03-03 11:57:27 -0800

    updated home page, added game data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,16 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-sans text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 text-blue-500">
-          Built using NextJS, Tailwind, and TypeScript&nbsp;
-          <code className="font-bold">by Andrew Byun</code>
+          Built using NextJS, Tailwind, and TypeScript
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-gray-700 via-gray-800 dark:from-gray-300 dark:via-gray-400 lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By {"Andrew Byun"}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
---
+++ src/app/scripts/seed.js
@@ -0,0 +1,8 @@
+const db = require('../lib/db.js');
+const {gamedata} = require('../lib/game-data.js');
+
+async function seed () {
+    // try {
+    //     const insertQuery = INSERT 
+    // }
+}
