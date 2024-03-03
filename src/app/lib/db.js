3081b39d62c546c51bf01c2a1ffdd4ad05c51870
Andrew Byun <andrewbyun1@gmail.com>
2024-03-03 11:56:04 -0800

    added game data, updated home page colors and text

---
+++ src/app/lib/db.js
@@ -0,0 +1,14 @@
+const pg = require('pg');
+const {Pool} = require('pg');
+var PG_URI = 'postgresql://andrewbyun1:0OxhaETfvq8I@ep-still-limit-a6sh2pmv-pooler.us-west-2.aws.neon.tech/gamelistDB?sslmode=require';
+
+const pool = new Pool({
+    connectionString: PG_URI
+});
+
+module.exports = {
+    query: (text, params, callback) => {
+        console.log('executed query', text);
+        return pool.query(text, params, callback);
+    }
+};
